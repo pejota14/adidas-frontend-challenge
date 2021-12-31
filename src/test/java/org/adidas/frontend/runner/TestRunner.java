e6b6cd4d296160058a1264ed4374c04af12abfe9
Pablo <pjfernandezcot@hotmail.com>
2021-12-31 19:01:06 +0100

    chore: add run with serenity to generate reports

--- src/test/java/org/adidas/frontend/runner/TestRunner.java
+++ src/test/java/org/adidas/frontend/runner/TestRunner.java
@@ -1,10 +1,10 @@
 package org.adidas.frontend.runner;
 
-import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = {
                 "src/test/resources/features"
